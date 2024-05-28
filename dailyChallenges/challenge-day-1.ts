b3c7b26c5932ed57021b0a0bb77dd2803b86e369
Pedro Nel Builes Moreno <pedronel.builes@gmail.com>
2024-05-28 15:35:17 -0500

    finish algorith from 28-05-2024

--- dailyChallenges/challenge-day-1.ts
+++ dailyChallenges/challenge-day-1.ts
@@ -9,3 +9,22 @@
  * La dificultad del ejercicio no recide en resolverlo sin mas, sino en resolverlo con un Time complexity no superior a O(n)
  */
 
+const same = (arrOne: number[], arrTwo: number[]) => {
+    console.time('same execution')
+    return arrOne.every(element => arrTwo.indexOf(element**2) > -1 ? arrTwo.splice(arrTwo.indexOf(element**2), 1) : false)
+}
+
+console.log("Array 1: ", same([1, 2, 3], [4, 1, 9]))
+console.timeEnd('same execution')
+
+console.log("Array 2: ", same([1, 2, 1], [4, 4, 1]))
+console.timeEnd('same execution')
+
+console.log("Array 3: ", same([1, 2, 2, 3], [6, 4, 1, 4]))
+console.timeEnd('same execution')
+
+console.log("Array 4: ", same([1, 8, 6, 3, 6, 10, 1], [100, 1, 1, 64, 36, 9, 36]))
+console.timeEnd('same execution')
+
+console.log("Array 5: ", same([1, 8, 6, 3, 7, 10, 1], [100, 1, 1, 64, 36, 9, 36]))
+console.timeEnd('same execution')
