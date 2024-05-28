8af38d7caed8cda40246f688ed661b0b37ee5243
Pedro Nel Builes Moreno <pedronel.builes@gmail.com>
2024-05-28 10:05:51 -0500

    Initial commit

---
+++ dailyChallenges/challenge-day-1.ts
@@ -0,0 +1,11 @@
+/**
+ * Crear una función llamada same, que recibe 2 arreglos como argumento, 
+ * la función debe verificar que cada valor en el primer arreglo tiene su correspondiente valor al cuadrado en el segundo arreglo, 
+ * debe retornar false si no cumple y true si cumple
+ * 
+ * ejemplo same ([1, 2, 3], [4, 1, 9]) // Retorna true
+ * same ([1, 2, 1], [4, 4, 1]) //  Retorna false porque cada valor necesita su propia pareja
+ * 
+ * La dificultad del ejercicio no recide en resolverlo sin mas, sino en resolverlo con un Time complexity no superior a O(n)
+ */
+
